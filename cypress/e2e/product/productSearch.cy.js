973085d434c5c81d82de80f1434a97b087bd7e73
Sandra Lourenco <sandralourenc@yahoo.com>
2024-10-03 23:21:44 +0100

    product tests

--- cypress/e2e/product/productSearch.cy.js
+++ cypress/e2e/product/productSearch.cy.js
@@ -2,25 +2,28 @@
 
 describe("Registered user should be able to login", () => {
   beforeEach(() => {
-    cy.visit("https://www.cotswoldoutdoor.com/");
+    cy.visit("/");
+    cy.wait(3000);
   });
 
   it("Log in as a registered user", () => {
     cy.login();
-    
   });
 
-
-it("Search for a product using the search bar", () => {
-    
-    
+  it("Search for a product using the search bar", () => {
+    cy.login();
+    cy.get('[data-qa="searchbox"]').click();
+    cy.get('[data-qa="searchbox"]').type("shirts{enter}");
   });
 
-it("Verify that the search results match the query", () => {
-    
-    
+  it("Verify that the search results match the query", () => {
+    cy.login();
+    //search for shirts
+    cy.get('[data-qa="searchbox"]').click();
+    cy.get('[data-qa="searchbox"]').type("shirts{enter}");
+    //check if result is correct
+    cy.get(".as-a-heading.as-a-heading--title").should("be.visible");
+    cy.url().should("include", "/shirts");
+    cy.get(".as-m-breadcrumb__list > :nth-child(4) > .as-a-text").should.exist;
   });
 });
-
-
-
