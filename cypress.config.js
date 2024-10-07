c6009537a409d2bc9b8eb280dad09939b914f16f
Sandra Lourenco <sandralourenc@yahoo.com>
2024-10-07 11:40:47 +0100

    report

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
     
