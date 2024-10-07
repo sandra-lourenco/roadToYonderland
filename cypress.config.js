d6dfcd1c7fa699c6ae2b537321440188d66df987
Sandra Lourenco <sandralourenc@yahoo.com>
2024-10-07 13:47:53 +0100

    folder

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: 'mochawesome',
   reporterOptions: {
-    reportDir: 'cypress/reports/mochawesome-report',
+    reportDir: 'cypress/reports/',
     overwrite: false,
     html: true,
     json: true
